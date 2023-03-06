5c2e8780acebe2005c144f8a4af97b7fb2b24164
Paul Dippold <pauldippold@me.com>
2023-03-05 20:38:37 -0500

    Initial Commit

---
+++ G2G/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  G2G
+//
+//  Created by Paul Dippold on 3/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ G2G/G2GApp.swift
@@ -0,0 +1,17 @@
+//
+//  G2GApp.swift
+//  G2G
+//
+//  Created by Paul Dippold on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct G2GApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
